1a685fde8fa95a1852924a93ca6c391af79baa61
red-t <2860257556@qq.com>
2021-11-24 22:18:09 +0800

    update all

--- TEMP2_with_slurm_hs37d5.sh
+++ TEMP2_with_slurm_hs37d5.sh
@@ -17,27 +17,8 @@ date
 echo ""
 
 
-######Help Information########
-function help_info(){
-    echo `basename $0`
-    echo -e "\t-f <filelist>\tfilelist of the BAM files that used for TEMP2 insertion2."
-    echo -e "\t-o <outdir>\toutput path."
-    echo -e "\t-h \tShow this information"
-}
-
-
-while getopts ":f:o:h" OPTION; do
-    case $OPTION in
-        f)  FILELIST=$OPTARG;;
-        o)  OUTPUTDIR=$OPTARG;;
-        h)  help_info && exit 1;;
-        *)  help_info && exit 1;;
-    esac
-done
-
-
 # List of input files
-[ -z $FILELIST ] && FILELIST=/data/tusers/zhongrenhu/for_SMS/dna_pipline/TEMP2_result/TEMP2_filelist
+[ -z $FILELIST ] && FILELIST=/data/tusers/zhongrenhu/for_SMS/dna_pipline/TEMP2_result/TEMP2_filelist1
 [ -z $OUTPUTDIR ] && OUTPUTDIR=/data/tusers/zhongrenhu/for_SMS/dna_pipline/TEMP2_result
 
 # Get files from FILELIST
@@ -46,7 +27,8 @@ str=`sed "${SLURM_ARRAY_TASK_ID}q;d" $FILELIST`
 # Read the SLURM_ARRAY_TASK_ID'th line from filelist
 # Split line into array
 toks=($str)
-INPUT=${toks[0]}
+LEFT=${toks[0]}
+RIGHT=${toks[1]}
 
 # Create temporary directory for work
 echo "Creating temporary working dir..."
@@ -57,15 +39,11 @@ echo ""
 
 # Copy inputs to temp dir
 echo "Copying input files to temp directory..."
-PREFIX=`basename ${INPUT%.bam}` && INPUT_DIR=`dirname ${INPUT}`
-cp $INPUT $INPUT.bai $INPUT_DIR/$PREFIX[_.][12].f*q* .
+PREFIX=`basename ${LEFT%_1.fastq.gz}` && INPUT_DIR=`dirname ${INPUT}`
+cp $LEFT $RIGHT .
 echo "Done."
 echo ""
 
-# Get filenames in temp by cutting of the path
-BAM=$(basename $INPUT)
-LEFT=$PREFIX[_.]1.f*q*
-RIGHT=$PREFIX[_.]2.f*q*
 
 # exporting environment variable and default parameter
 export PATH=/data/tusers/zhongrenhu/Software/gawk-5.1.0/opt/bin:$PATH
@@ -81,13 +59,13 @@ TE_BED6=${PATH_ANNO}/ALUL1SVA.bed
 # Process the data
 echo "Running TEMP2 insertion for ${BAM}"
 mkdir ${PREFIX}
-TEMP2 insertion -l ${LEFT} -r ${RIGHT} -i ${BAM} -g ${GENOME_FA} -R ${TE_FA} -t ${TE_BED6} -o ${PREFIX} -p ${PREFIX} -c 32 -M 2 -m 10 -U 0.95 -S -A #-d
+TEMP2 insertion -l ${LEFT} -r ${RIGHT} -I ${BWA_INDEX} -g ${GENOME_FA} -R ${TE_FA} -t ${TE_BED6} -o ${PREFIX} -p ${PREFIX} -c 32 -M 2 -m 10 -U 0.95 -S -A -d
 echo ""
 
 # Copy files to output dir
 echo "Copying results to destination..."
 ls -lh
-rm ${BAM} ${LEFT} ${RIGHT}
+rm ${LEFT} ${RIGHT}
 cp -r ${PREFIX} $OUTPUTDIR/
 echo "Done."
 echo ""
